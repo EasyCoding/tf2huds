580d283e7ff02ca78a9d391423892662466920e5
Vitaly Zaitsev <vitaly@easycoding.org>
2022-08-25 13:37:47 +0200

    Updated huds_update.sh.

--- huds_update.sh
+++ huds_update.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 set -e
 
-TAGNAME="archive"
-TAGDATE=$(date +%d-%m-%Y)
+TAGNAME=$(date +%Y.%m.%d)
+TAGDATE=$(date +%Y-%m-%d)
 
 echo "Initializing submodules..."
-git submodule init > /dev/null 2>&1
+git submodule init
 
 echo "Updating submodules from remote repositories..."
-git submodule update --recursive --remote > /dev/null 2>&1 || true
+git submodule update --recursive --remote
 sleep 10
 
 echo "Committing changes..."
-git commit -a -m "Updated all HUDs to the latest releases." > /dev/null 2>&1
+git commit -a -m "Updated all HUDs to the latest releases."
 
 echo "Removing existing Git tag..."
 git tag --delete "$TAGNAME" > /dev/null 2>&1 || true
 git push --delete origin "$TAGNAME" > /dev/null 2>&1 || true
 
 echo "Adding new tag..."
-git tag -s "$TAGNAME" -m "Release of $TAGDATE." > /dev/null 2>&1
+git tag -s "$TAGNAME" -m "Release of $TAGDATE."
 
 echo "Pushing changes..."
-git push -u origin master > /dev/null 2>&1
-git push -u origin --tags > /dev/null 2>&1
+git push -u origin master
+git push -u origin --tags
 
 echo "Completed."
